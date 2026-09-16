e460ab7b1410807cd84fa4e12c4dde546fc4f5fa
chukkavarshita <chukkavarshita@gmail.com>
2026-09-16 20:24:06 +0530

    Create FindMin.java

---
+++ Assignments/FindMin.java
@@ -0,0 +1,11 @@
+class FindMin {
+    public int findMin(int[] nums) {
+        int min=nums[0];
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]<min)
+            min=nums[i];
+        }
+        return min;
+    }
+}
