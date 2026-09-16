e266eacf32049c42dbd92e095b38435c36413197
chukkavarshita <chukkavarshita@gmail.com>
2026-09-16 20:16:39 +0530

    Create Fibonacci.java

---
+++ Assignments/Fibonacci.java
@@ -0,0 +1,14 @@
+class Fibonacci {
+    public int fib(int n) {
+        if(n<=0)
+        return n;
+        int a=0,b=1;
+        for(int i=2;i<=n;i++)
+        {
+            int temp=b;
+            b=a+b;
+            a=temp;
+        }
+        return b;
+    }
+}
