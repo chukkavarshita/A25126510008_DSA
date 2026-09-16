b2d3c721f5026c94a33ae623a74478a7e3c99999
chukkavarshita <chukkavarshita@gmail.com>
2026-09-16 20:19:36 +0530

    Create MoveZeros.java

---
+++ Assignments/MoveZeros.java
@@ -0,0 +1,15 @@
+class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        int i,j=0;
+        for(i=0;i<nums.length;i++)
+        {
+                if (nums[i] != 0) 
+                {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+                }
+        }
+    }
+}
